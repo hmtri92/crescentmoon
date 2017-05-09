521537ff74ebb35f0172951bd2571a043925ba75
Minh Tri <Minh Tri>
2017-05-10 00:50:15 +0700

    first commit

---
+++ src/main/java/in/crescentmoon/CrescentmoonWebApplication.java
@@ -0,0 +1,12 @@
+package in.crescentmoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrescentmoonWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrescentmoonWebApplication.class, args);
+	}
+}
